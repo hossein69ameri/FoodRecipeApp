c09f54a8e7d8799e40e5bd744550b09636bdf7be
Hossein <hossein.arabameri69@gmail.com>
2023-03-16 19:15:47 +0330

    DI For Network

---
+++ app/src/main/java/com/example/recipeapp/data/network/ApiServices.kt
@@ -0,0 +1,9 @@
+package com.example.recipeapp.data.network
+
+
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiServices {
+
+}
---
+++ app/src/main/java/com/example/recipeapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.recipeapp.util
+
+object Constants {
+    const val BASE_URL = "https://api.spoonacular.com/"
+    const val CONNECTION_TIME = 60L
+}
