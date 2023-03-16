e3422a451f3548c2a9a5e4b5043c53922089785e
Hossein <hossein.arabameri69@gmail.com>
2023-03-16 17:16:51 +0330

    Design RegisterFragment

---
+++ app/src/main/java/com/example/recipeapp/ui/register/RegisterFragment.kt
@@ -0,0 +1,17 @@
+package com.example.recipeapp.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.R
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
