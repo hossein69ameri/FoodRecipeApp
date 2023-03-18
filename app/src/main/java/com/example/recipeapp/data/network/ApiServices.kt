de04c4e5ef2404adf2b507bfee5e92909366b1a9
Hossein <hossein.arabameri69@gmail.com>
2023-03-18 17:04:16 +0330

    Add Model Class for Register

--- app/src/main/java/com/example/recipeapp/data/network/ApiServices.kt
+++ app/src/main/java/com/example/recipeapp/data/network/ApiServices.kt
@@ -1,9 +1,20 @@
 package com.example.recipeapp.data.network
 
 
+import com.example.recipeapp.model.BodyRegister
+import com.example.recipeapp.model.ResponseRegister
+import com.example.recipeapp.util.Constants
+import com.example.recipeapp.util.Constants.API_KEY
 import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiServices {
+
+    @POST("users/connect")
+    suspend fun postRegister(
+        @Query(API_KEY) apiKey: String,
+        @Body body: BodyRegister
+    ): Response<ResponseRegister>
+
 
 }
--- app/src/main/java/com/example/recipeapp/util/Constants.kt
+++ app/src/main/java/com/example/recipeapp/util/Constants.kt
@@ -3,4 +3,8 @@ package com.example.recipeapp.util
 object Constants {
     const val BASE_URL = "https://api.spoonacular.com/"
     const val CONNECTION_TIME = 60L
+    const val SITE_API_KEY = "f540b3d94dbc448bb277a260b08d16d1"
+    const val API_KEY = "apiKey"
+
+
 }
