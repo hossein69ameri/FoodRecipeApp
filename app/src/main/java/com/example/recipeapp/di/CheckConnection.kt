5ce5ca221aa5bdc863af4a138d10a776fc3ea0ba
Hossein <hossein.arabameri69@gmail.com>
2023-03-18 16:25:43 +0330

    Add NetworkChecker class

---
+++ app/src/main/java/com/example/recipeapp/di/CheckConnection.kt
@@ -0,0 +1,39 @@
+package com.example.recipeapp.di
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CheckConnection {
+
+
+    @Provides
+    @Singleton
+    fun provideCM(@ApplicationContext context: Context) =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    @Provides
+    @Singleton
+    fun provideRQ(): NetworkRequest = NetworkRequest.Builder().apply {
+        addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        //android m
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        //android p
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+            addCapability(NetworkCapabilities.NET_CAPABILITY_FOREGROUND)
+    }.build()
+
+}
